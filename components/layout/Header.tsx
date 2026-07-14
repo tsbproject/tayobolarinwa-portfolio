3bee62640b26427e041dabe4d9e7f70917b63a50
TSB <122724396+tsbproject@users.noreply.github.com>
2026-07-14 18:47:10 +0100

    [commit]

--- components/layout/Header.tsx
+++ components/layout/Header.tsx
@@ -98,7 +98,7 @@ export default function Header() {
           <div className="hidden items-center gap-4 lg:flex">
 
               <Button
-                href="/resume/resume.pdf"
+                href="/resume/tayo-bolarinwa-resume.pdf"
                 target="_blank"
                 variant="secondary"
               >
@@ -168,7 +168,7 @@ export default function Header() {
           <div className="space-y-4">
 
            <Button
-              href="/resume/Tayo-Bolarinwa-Resume.pdf"
+                href="/resume/tayo-bolarinwa-resume.pdf"
               target="_blank"
               variant="secondary"
             >
